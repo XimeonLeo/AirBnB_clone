cf7830805e1a2e126400d114f386eeff152d985b
Adetops <heritagediwamu@gmail.com>
2023-08-10 14:32:03 +0100

    File storage and engine

--- A_tops/models/base.py
+++ A_tops/models/base.py
@@ -5,34 +5,33 @@ Module that defines the class BaseModel
 
 from datetime import datetime
 import uuid
-
+from models import storage
 
 class BaseModel:
     """ This class defines all common attributes/methods for other classes """
 
     def __init__(self, *args, **kwargs):
-        if (kwargs is not None):
+        if len(kwargs):
             for k, v in kwargs.items():
                 if (k != "__class__"):
                     if (k == "created_at") or (k == "updated_at"):
-                        kwargs[k] = datetime.fromisoformat(v)
+                        self.__dict__[k] = datetime.fromisoformat(v)
                     setattr(self.__class__, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        dt = self.__dict__
+        dt = self.__dict__.copy()
         dt["__class__"] = self.__class__.__name__
         for k, v in dt.items():
             if (k == "created_at"):
---
+++ A_tops/models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Defines the FileStorage class that serializes and deserializes """
+
+import json
+from models.base import BaseModel
+
+class FileStorage:
+    """ serializes and deserializes an instance to a file and vice versa """
+
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        return (self.__objects)
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            obj_ = {k: v.to_dict() for k, v in self.__objects.items()}
+            json.dump(obj_, f)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, mode="r") as fh:
+                load_ = json.load(fh)
+                for v in load.values():
+                    self.new(eval(v["__class__"])(**v))
+        except FileNotFoundError:
+            pass
