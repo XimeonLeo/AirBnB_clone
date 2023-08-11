46670782eba7854c686ba6a9e4abe99d303967c4
XimeonLeo <ximeonleo@gmail.com>
2023-08-11 11:01:56 +0100

    more updates

--- Leo/console.py
+++ Leo/console.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """ Defines a subclass that inherits from cmd.Cmd """
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
+
+allClasses = ["BaseModel"]
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +24,82 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, cmd):
+        """Creates a new instance of BaseModel, saves it
+            (to the JSON file) and prints the id..
+            Ex: $ create BaseModel
+        """
+        args = cmd.split()
+
+        if len(args) == 0:
+            print(f"** class name missing **")
+        elif args[0] not in allClasses:
+            print(f"** class doesn't exist **")
+        else:
+            MyModel = eval(args[0])()
+            MyModel.save()
+            print(MyModel.id)
+
+    def do_show(self, cmd):
+        """Prints the string representation of an instance
+            based on the class name and id.
+            Ex: $ show BaseModel 1234-1234-1234.
+        """
+        args = cmd.split()
+
+        if len(args) == 0:
+            print(f"** class name missing **")
+        elif args[0] not in allClasses:
+            print(f"** class doesn't exist **")
+        elif len(args) == 1:
+            print(f"** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print(f"** no instance found **")
+            else:
+                print(storage.all()[key])
+
+    def do_destroy(self, cmd):
+        """Deletes an instance based on the class name and id
+            (save the change into the JSON file).
+            Ex: $ destroy BaseModel 1234-1234-1234.
+        """
+        args = cmd.split()
+
+        if len(args) == 0:
+            print(f"** class name missing **")
+        elif args[0] not in allClasses:
+            print(f"** class doesn't exist **")
+        elif len(args) == 1:
+            print(f"** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print(f"** no instance found **")
+            else:
+                del(storage.all()[key])
+
+    def do_all(self, cmd):
+        """ Prints all string representation of all instances
+            based or not on the class name.
+            Ex: $ all BaseModel or $ all
+        """
+        args = cmd.split()
+
+        if len(args) == 0:
+            print([str(v) for v in storage.all().values()])
+        elif args[0] not in allClasses:
+            print(f"** class doesn't exist **")
+        else:
+            lst = []
+            for k, v in storage.all().items():
+                if k.startswith(args[0]):
+                    lst.append(str(v))
+            print(lst)
+        # print([str(v) for k, v in storage.all().items()
+        # if k.startswith(args[0])])
 
 
 if __name__ == '__main__':
