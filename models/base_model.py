652fda61f33a54053874f7f71dca82cb96402e21
XimeonLeo <ximeonleo@gmail.com>
2023-08-13 18:07:37 +0100

    Searching for bugs

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,8 @@ class BaseModel():
                     self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
 
     def __str__(self):
@@ -33,7 +33,7 @@ class BaseModel():
 
     def save(self):
         """ updates attribute <updated_at> with cuerent DT """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
